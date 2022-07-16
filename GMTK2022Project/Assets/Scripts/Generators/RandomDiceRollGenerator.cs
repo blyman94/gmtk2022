d441489bbdc161934bea4fb4a5a8091889103595
Kane Xu <71009614+xuhongkang@users.noreply.github.com>
2022-07-16 00:43:05 -0400

    9:00

--- GMTK2022Project/Assets/Scripts/Generators/RandomDiceRollGenerator.cs
+++ GMTK2022Project/Assets/Scripts/Generators/RandomDiceRollGenerator.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,7 +13,9 @@ public class RandomDiceRollGenerator : MonoBehaviour
     public int tarRollValue;
     public int minRollValue;
     public int maxRollValue;
+    public int rotateAngle;
 
+    public Transform pivot;
     public GameObject faceNumber;
 
 
@@ -36,8 +39,22 @@ public class RandomDiceRollGenerator : MonoBehaviour
         for (int i = 0; i < numOfRolls; i++) {
             int tempTar = Random.Range(minRollValue, maxRollValue + 1);
             faceNumber.GetComponent<Image>().sprite = faceNumberSprites[tempTar - 1];
-            yield return new WaitForSecondsRealtime(waitTime);
+            yield return Spin(new Quaternion(0, 0, Random.Range(-2, 3) * rotateAngle, 1));
+            //yield return new WaitForSecondsRealtime(waitTime);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, new Quaternion(0, 0, 90, 1), waitTime/90 * Time.deltaTime);
         }
+        yield return Spin(new Quaternion(0, 0, 0, 1));
         faceNumber.GetComponent<Image>().sprite = faceNumberSprites[tarRollValue - 1];
+    }
+
+    IEnumerator Spin(Quaternion rotation)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime <= waitTime)
+        {
+            pivot.rotation = Quaternion.Lerp(pivot.rotation, rotation, elapsedTime / waitTime);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForSecondsRealtime(0f);
+        }
     }
 }
