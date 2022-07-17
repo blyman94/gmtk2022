3d7fabe19edfcd3a861c9744951682cdbf23287f
Warspiteful <jalmendral24@gmail.com>
2022-07-17 10:21:53 -0400

    Patient Increasing Per Day

---
+++ GMTK2022Project/Assets/Scripts/Utilities/PatientIncrementor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientIncrementor : MonoBehaviour
+{
+    [SerializeField] private IntVariable dayCount;
+    [SerializeField] private IntVariable patientCount;
+    
+    private void OnEnable()
+    {
+        dayCount.ValueUpdated += calculatePatients;
+    }
+    
+    private void OnDisable()
+    {
+        dayCount.ValueUpdated -= calculatePatients;
+    }
+
+    private void calculatePatients()
+    {
+        if(dayCount.Value < 2)
+        {
+            patientCount.Value = 3;
+        }
+        else if (dayCount.Value < 4)
+        {
+            patientCount.Value = 4;
+        }
+        else if (dayCount.Value < 6)
+        {
+            patientCount.Value = 5;
+        }
+        else
+        {
+            patientCount.Value = 6;
+        }
+    }
+}
